2763f66f1e1eaad2803d6dd5e7577b80325d8f82
zakaria ali <alizakaria9375@gmail.com>
2023-12-30 12:37:37 +0200

    saving1

--- src/main.js
+++ src/main.js
@@ -1,33 +1,34 @@
-import { Client } from 'node-appwrite';
+import { Client, Databases } from "node-appwrite";
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+	const client = new Client()
+		.setEndpoint(process.env.APPWRITE_ENDPOINT)
+		.setProject(process.env.APPWRITE_PROJECT_ID)
+	log("Function is triggered");
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+	const database = new Databases(client);
+	let payload;
+	try {
+		payload = JSON.parse(req.bodyRaw);
+	} catch (e) {
+		error("Error parsing JSON:", e);
+		return res.json({ error: "Invalid JSON format" });
+	}
+	const userId = payload.userId;
+	const isActive = payload.isActive;
+	const lastLoginTime = new Date().toISOString();
+	log("Payload is", payload);
+	log("lastLoginTime is", lastLoginTime);
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
+	try {
+		const response = await database.updateDocument("appData", "users", userId, {
+			isActive: isActive,
+			lastVisitAt: lastLoginTime,
+		});
+		log("User status updated:", response);
+		return res.json(response);
+	} catch (e) {
+		error("Error updating user status:", e);
+		return res.json({ error: e.message });
+	}
 };
