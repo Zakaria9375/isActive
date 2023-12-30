18469ac4eefbc3620a908196ca42941fe3198e6c
zakaria ali <alizakaria9375@gmail.com>
2023-12-30 14:38:19 +0200

    saving2

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@ import { Client, Databases } from "node-appwrite";
 export default async ({ req, res, log, error }) => {
 	const client = new Client()
 		.setEndpoint(process.env.APPWRITE_ENDPOINT)
-		.setProject(process.env.APPWRITE_PROJECT_ID)
+		.setProject(process.env.APPWRITE_PROJECT_ID);
 	log("Function is triggered");
 
 	const database = new Databases(client);
+	log(req.bodyRaw);
 
+	log(req.bodyRaw.userId, req.bodyRaw.isActive);
 	const userId = req.bodyRaw.userId;
 	const isActive = req.bodyRaw.isActive;
 	const lastLoginTime = new Date().toISOString();
-	log("Payload is", userId, isActive);
-	log("lastLoginTime is", lastLoginTime);
 
 	try {
 		const response = await database.updateDocument("appData", "users", userId, {
